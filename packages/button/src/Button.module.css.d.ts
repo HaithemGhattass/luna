7ace4c1aafc9d010fcdf701001b2c2d1f3b2cebd
HaithemGhattass <haithem.ghattas@esprit.tn>
2026-01-06 00:41:01 +0100

    feat: creeate button package

---
+++ packages/button/src/Button.module.css.d.ts
@@ -0,0 +1,23 @@
+declare const styles: {
+  readonly button: string
+  readonly primary: string
+  readonly secondary: string
+  readonly subtle: string
+  readonly ghost: string
+  readonly outline: string
+  readonly destructive: string
+  readonly link: string
+  readonly xs: string
+  readonly sm: string
+  readonly md: string
+  readonly 'default-size': string
+  readonly lg: string
+  readonly icon: string
+  readonly loading: string
+  readonly spinner: string
+  readonly fullWidth: string
+  readonly iconLeft: string
+  readonly iconRight: string
+}
+
+export default styles
